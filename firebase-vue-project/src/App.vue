86a8f0fca32c527cba5ffe7b5bfb55d61f192cec
kboysm <lishal12@naver.com>
2020-08-20 21:07:56 +0900

    drawer

--- firebase-vue-project/src/App.vue
+++ firebase-vue-project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app color="deep-purple accent-4" dense dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Page title</v-toolbar-title>
 
@@ -15,6 +15,25 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" class="deep-purple accent-4" dark>
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block>Logout</v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
     <v-content>
       <router-view />
     </v-content>
@@ -30,7 +49,12 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    drawer: false,
+    items: [
+      { title: "Dashboard", icon: "dashboard" },
+      { title: "Account", icon: "account_box" },
+      { title: "Admin", icon: "gavel" },
+    ],
   }),
 }
 </script>
