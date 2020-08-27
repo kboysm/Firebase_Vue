1a90f5c09f9ea457826a37662c9a3af79c658084
kboysm <lishal12@naver.com>
2020-08-27 22:38:48 +0900

    firebase writeAndRead

--- firebase-vue-project/src/App.vue
+++ firebase-vue-project/src/App.vue
@@ -4,6 +4,15 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <site-title :title="title"></site-title>
       <v-spacer></v-spacer>
+      <v-btn icon @click="save">
+        <v-icon>mdi-check</v-icon>
+      </v-btn>
+      <v-btn icon @click="read">
+        <v-icon>mdi-read</v-icon>
+      </v-btn>
+      <v-btn icon @click="readOne">
+        <v-icon>mdi-numeric</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer" dark>
       <site-menu />
@@ -37,5 +46,38 @@ export default {
     footer: "testFooter",
     drawer: false,
   }),
+  created() {
+    console.log(this.$firebase)
+  },
+  methods: {
+    save() {
+      this.$firebase.database().ref().child("abcd").set({
+        //ref는 root , 차일드로 abcd 주입 set안에는 object가 들어감
+        title: "abcd",
+        text: "iLikeVuejs",
+      })
+    },
+    read() {
+      //realtime의 read는 2가지가 존재
+      // 1번만 읽는다! , 파이프를 연결해 놓고 계속 읽느냐! 2가지
+      this.$firebase
+        .database()
+        .ref()
+        .child("abcd")
+        .on("value", (sn) => {
+          console.log(sn)
+          console.log(sn.val())
+        }) //이건 파이프를 연결해서 값이 변경하면 자동으로 바뀐값이 DB에서 넘어옴 완전 신세계
+      //잘 활용하면 다이나믹한 자동으로 마구마구 바뀌는 기능을 구현할 수 있다
+    },
+    async readOne() {
+      const sn = await this.$firebase
+        .database()
+        .ref()
+        .child("abcd")
+        .once("value")
+      console.log(sn.val())
+    },
+  },
 }
 </script>
